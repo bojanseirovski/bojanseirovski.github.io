0fdc31aed68d833dd94fffe7a3d3df6fc2e8b8c0
Bojan Seirovski <druid0101@gmail.com>
2023-10-13 23:44:52 -0400

    ongoing work, account, login...

---
+++ src/components/Account.tsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+
+const Account = () => {
+
+    return (
+        <div className="coloringWrapper mt-4">
+            
+        </div>
+    );
+}
+
+export default Account;
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+
+const Dashboard = () => {
+
+    return (
+        <div className="coloringWrapper mt-4">
+            
+        </div>
+    );
+}
+
+export default Dashboard;
